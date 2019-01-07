bc81bf8a15d71e8aa97072c2e39c3f48bd9d690a
Seshan Ravikumar <seshan10@me.com>
2019-01-07 14:26:52 -0500

    test

--- README.md
+++
@@ -1,4 +0,0 @@
-# Connect4
-For an ICS4U project (Grade 12 Computer Science)
-
-Deep4 is a GUI Connect4 implementation in Java, that uses a neural network as the AI.
--- src/HelperMethods.java
+++
@@ -1,9 +0,0 @@
-public class HelperMethods {
-    public static boolean debugMode = false;
-    public static void debugPrintln(String s) {
-        // This method will print a message only if debug mode is on.
-        if(debugMode){
-            System.out.println("[DEBUG] " + s);
-        }
-    }
-}
