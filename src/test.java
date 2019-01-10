8f12c0d27095437ac7c1a9284f228b350b1fc63a
Jack <jack.d1ngr.li@gmail.com>
2019-01-09 19:20:43 -0800

    Stuff

--- src/test.java
+++ src/test.java
@@ -5,104 +5,41 @@ public class test {
     static boolean isDebug = true;
     public static void main(String args[]) {
         int[][] testGrid =  new int[][] {
-            {0, 1, 2, 2, 2, 2, 1},
-            {},
-            {},
-            {},
-            {},
-            {}
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 2, 1, 2, 0, 0},
+            {0, 1, 2, 1, 1, 2, 0}
         };
-        int currentPlayerID = 2;
-        int[] userInput = new int[] {4,5};
+        int currentPlayerID = 1;
+        int[] userInput = new int[] {2, 3};
 
         System.out.println(checkerFunctionMethod(testGrid, userInput, currentPlayerID));
     }
 
     public static boolean checkerFunctionMethod(int[][] seshanIsLazy, int[] inputCord, int currentID) {
-        boolean connect4 = false;
-        String[][] stringGrid = new String[7][7];
-        for (int i = 0; i < seshanIsLazy.length; i++) {
-            for (int j = 0; j < seshanIsLazy[i].length; j++) {
-                if (seshanIsLazy[i][j] == 1) {
-                    stringGrid[i][j] = "a";
-                } if (seshanIsLazy[i][j] == 2) {
-                    stringGrid[i][j] = "b";
-                } else {
-                    stringGrid[i][j] = "n";
-                }
-            }
-        }
+
+        if(isDebug) System.out.println(Arrays.toString(seshanIsLazy));
 
         String tempHor = "";
         String tempVer = "";
-        String tempDiaA = "";
-        String tempDiaB = "";
+        String tempDiaA = stringDiagonalA(inputCord, seshanIsLazy);
+        String tempDiaB = stringDiagonalB(inputCord, seshanIsLazy);
 
         for (int i = 0; i < 7; i++) {
-            tempHor += stringGrid[inputCord[0]][i];
+            tempHor += Integer.toString(seshanIsLazy[inputCord[0]][i]);
+            if(isDebug) System.out.println(tempHor);
         }
         for (int i = 0; i < 6; i++) {
-            tempVer += stringGrid[i][inputCord[1]];
+            tempVer += Integer.toString(seshanIsLazy[i][inputCord[1]]);
+            if(isDebug) System.out.println(tempVer);
         }
-        int count = 0;
-        
-        int x = 0;
-        int y = 0;
-        
-        //Going NW
-        count = 0;
-        do {
-            y = inputCord[0];
-            x = inputCord[1];
-            tempDiaA = stringGrid[y][x] + tempDiaA;
-            count++;
-            y -= count;
-            x -= count; 
-        } while (x < 0 || y < 0);
 
-        //Going SE
-        count = 0;
-        do {
-            y = inputCord[0];
-            x = inputCord[1];
-            count++;
-            x = x + count;
-            y = y + count;
-            while(isDebug) System.out.println(x + ", " + y + ", " + tempDiaA);
-            tempDiaA = tempDiaA + stringGrid[y][x];
-            while(isDebug) System.out.print(tempDiaA);
-        } while (y < 6 || x < 7);
+        if(isDebug) System.out.println(tempDiaA);
+        boolean connect4 = false;
 
-        //Going NE
-        count = 0;
-        do {
-            y = inputCord[0];
-            x = inputCord[1];
-            count++;
-            y -= count;
-            x += count;
-            tempDiaB = tempDiaB + stringGrid[y][x];
-        } while (x < 7 || y < 6);
-
-        //Going SW
-        count = 0;
-        do {
-            y = inputCord[0];
-            x = inputCord[1];
-            tempDiaB = stringGrid[y][x] + tempDiaB;
-            count++;
-            y += count;
-            x -= count;
-        } while (x < 7 || y < 6);
-
-        String playerID = "";
-        if (currentID == 1) {
-            playerID = "a";
-        } else {
-            playerID = "b";
-        }
-        
-        if (repeatCounter(playerID, tempDiaA) == 4 || repeatCounter(playerID, tempDiaB) == 4 || repeatCounter(playerID, tempHor) == 4 || repeatCounter(playerID, tempVer) == 4) {
+        if (repeatCounter(currentID, tempDiaA) || repeatCounter(currentID, tempDiaB) || repeatCounter(currentID, tempHor) || repeatCounter(currentID, tempVer)) {
             connect4 = true;
         } else {
             connect4 = false;
@@ -112,16 +49,43 @@ public class test {
     }
 
     //To check the connect 4
-    public static int repeatCounter(String playerID, String stringTOCheck) {
-        int counter = 1;
-        char currentID = playerID.charAt(0);
-        for (int i = 0; i < stringTOCheck.length()-1; i ++) {
-            if (stringTOCheck.charAt(i) == stringTOCheck.charAt(i+1) && stringTOCheck.charAt(i) == currentID) {
-                counter++;
-            } else {
-                counter = 1;
-            }
+    public static boolean repeatCounter(int playerID, String stringTOCheck) {
+        String ID = Integer.toString(playerID);
+        String targetString = "";
+        for (int i = 0; i < 4; i++) {
+            targetString += ID;
         }
-        return counter;
+        
+        return stringTOCheck.matches("(.*)" + targetString +"(.*)");
+    }
+
+    public static String stringDiagonalA (int[] inputCord, int[][] seshanIsLazy) {
+        int x = inputCord[1] - inputCord[0];
+        int y = inputCord[0] - inputCord[0];
+        String finalString = "";
+
+        while(y < 6) {
+            finalString += seshanIsLazy[y][x];
+            y++;
+            x++;
+            if(isDebug) System.out.println(y + " " + x + " " + finalString);
+        }
+
+        return finalString;
+    }
+
+    public static String stringDiagonalB (int[] inputCord, int[][] seshanIsLazy) {
+        int x = inputCord[1] - (6 - inputCord[0]);
+        int y = inputCord[0] + (6 - inputCord[0]) - 1;
+        String finalString = "";
+
+        while(x < 7) {
+            finalString += seshanIsLazy[y][x];
+            y++;
+            x++;
+            if(isDebug) System.out.println(y + " " + x + " " + finalString);
+        }
+
+        return finalString;
     }
 }
